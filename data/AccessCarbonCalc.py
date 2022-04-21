0e6d4e446b1969b292a3337e0f813268889d9780
Aarushi1104 <tiwari.ishi@gmail.com>
2022-04-21 21:00:04 +1000

    Update AccessCarbonCalc.py

--- data/AccessCarbonCalc.py
+++ data/AccessCarbonCalc.py
@@ -10,12 +10,10 @@ driver.get('https://www.carbonfootprint.com/calculator.aspx')
 frame_0 = driver.find_element_by_id('cphContent_ifrCalc')
 driver.switch_to.frame(frame_0)
 select = Select(driver.find_element_by_id(r"ctl05_cczLocation_ddlCountry"))
-print(select)
 country = input("Country: ").upper()
 country = country[0] + country[1:].lower()
 select.select_by_visible_text(country)
 driver.find_element_by_id('btnNextTab').click()
-
 
 #house
 cell_id = {"Electricity usage in KWH: " : "ctl05_chsHouse_txtElecUsage",
@@ -29,4 +27,33 @@ for i in cell_id:
     into = driver.find_element_by_id(cell_id[i])
     consumption = input(i)
     into.send_keys(consumption)
+driver.find_element_by_id('btnNextTab').click()
+
+#flights
+yn = input("Taken any flights?")
+count = 0
+while yn.lower()[0] == 'y' and count < 3:
+    select = (driver.find_element_by_id(r"ctl05_rcbAirportFrom_Input"))
+    count += 1
+    oneway = input("One way? (y/n)")
+    if oneway.lower()[0] == 'y':
+        driver.find_element_by_id("ctl05_rbnOneWay").click()
+    loc = input("Takeoff airport code: ")
+    select.send_keys(loc + "\n")
+    loc = input("Landing airport code: ")
+    select = (driver.find_element_by_id(r"ctl05_rcbAirportTo_Input"))
+    select.send_keys(loc + "\n")
+    driver.find_element_by_class_name("calcbutton").click()
+    yn = input("more?")
+driver.find_element_by_id('btnNextTab').click()
+
+#car
+km = input("by car: ")
+into = driver.find_element_by_id("ctl05_cdsCar_txtMileage")
+into.send_keys(km)
+select = (driver.find_element_by_id("ctl05_cefCar_txtEfficiency"))
+select.send_keys('171.48')
+select = Select(driver.find_element_by_id("ctl05_cefCar_ddlEfficiencyUnit"))
+select.select_by_visible_text("g/km")
+driver.find_element_by_class_name("calcbutton").click()
 driver.find_element_by_id('btnNextTab').click()
