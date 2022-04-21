63d357a15bbfec106f98460fce766ea27f8db98f
benhsampson <benhsampson@protonmail.com>
2022-04-21 15:51:39 +1000

    add template

---
+++ frontend/pages/template.js
@@ -0,0 +1,13 @@
+import { Button, Container } from '@mui/material'
+import Header from '../components/Header'
+
+const Template = () => (
+  <>
+    <Header />
+    <Container>
+      <Button>TEMPLATE</Button>
+    </Container>
+  </>
+)
+
+export default Template
